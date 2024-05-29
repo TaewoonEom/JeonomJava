6d4976f734991dc8279f8a66ac57bc3efe6adc00
Taewoon Eom <thtw28@gmail.com>
2024-05-29 18:01:33 +0900

    [ADD] 24/05/29 Java 9일차 객체지향원칙 3,다형성
    1. ‘여러개의형태를갖는다’는의미, 객체지향 프로그래밍의 3대 특징 중 하나
    2. 상속을 이용한 기술로 Upcasting 활용
     - 부모타입의 변수로 자식 객체를 다룰 수 있음.
     - Super s = new Sub();
     - s는 부모의 필드와 메소드에만 접근할 수 있음.
    3. 다형성의 종류
     - 오버라이딩 : 모두 같으며 실행문 재정의
     - 오버로딩 : 이름만 같으며 매개변수 갯수와 타입 상이

---
+++ src/org/Jeonom/day09/oop/overriding/Circle.java
@@ -0,0 +1,9 @@
+package org.Jeonom.day09.oop.overriding;
+
+public class Circle extends Shape {
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Circle");
+	}
+}
